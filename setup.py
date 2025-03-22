24fefdc47bba9c1740906ea739ae21f0064dfb47
Muhammad Adil Naeem <162784706+muhammadadilnaeem@users.noreply.github.com>
2025-03-22 12:34:25 +0500

    Add setup.py for package configuration

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+    
+setup(
+    name="src",
+    version="0.0.1",
+    author="Adil Naeem",
+    author_email="madilnaeem0@gmail.com",
+    packages=find_packages()
+)
